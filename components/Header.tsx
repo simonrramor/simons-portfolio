4d2f89943fb0a1ba372647380f253ef145ddacf6
simon amor <simonrramor@gmail.com>
2026-01-24 20:32:49 +0000

    Update bio with company links

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,23 @@ export default function Header() {
       
       <div className={styles.bio}>
         <p className={styles.bioText}>
-          London based designer and Co-Founder of Sling Money
+          London based designer and Co-Founder of{' '}
+          <a href="https://slingmoney.com" target="_blank" rel="noopener noreferrer" className={styles.bioLink}>
+            Sling Money
+          </a>
+          . I was previously building at{' '}
+          <a href="https://spotify.com" target="_blank" rel="noopener noreferrer" className={styles.bioLink}>
+            Spotify
+          </a>
+          ,{' '}
+          <a href="https://monzo.com" target="_blank" rel="noopener noreferrer" className={styles.bioLink}>
+            Monzo
+          </a>
+          ,{' '}
+          <a href="https://google.com" target="_blank" rel="noopener noreferrer" className={styles.bioLink}>
+            Google
+          </a>
+          {' '}and more.
         </p>
       </div>
     </header>
